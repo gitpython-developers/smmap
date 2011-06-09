8d64e74ed80f2818acad652a69615708f8f61104
Sebastian Thiel <byronimo@gmail.com>
2011-06-10 00:38:48 +0200

    Added very special purpose method to free memory maps. The whole reason for this is to make the windows test work after all \!

--- smmap/mman.py
+++ smmap/mman.py
@@ -426,3 +426,32 @@ class MappedMemoryManager(object):
 		return PAGESIZE
 		
 	#} END interface
+	
+	#{ Special Purpose Interface
+	
+	def force_map_handle_removal_win(self, base_path):
+		"""ONLY AVAILABLE ON WINDOWS
+		On windows removing files is not allowed if anybody still has it opened.
+		If this process is ourselves, and if the whole process uses this memory
+		manager (as far as the parent framework is concerned) we can enforce
+		closing all memory maps whose path matches the given base path to 
+		allow the respective operation after all.
+		The respective system must NOT access the closed memory regions anymore !
+		This really may only be used if you know that the items which keep 
+		the cursors alive will not be using it anymore. They need to be recreated !
+		:return: Amount of closed handles
+		:note: does nothing on non-windows platforms"""
+		if sys.platform != 'win32':
+			return
+		#END early bailout
+		
+		num_closed = 0
+		for path, rlist in self._fdict.iteritems():
+			if path.startswith(base_path):
+				for region in rlist:
+					region._mf.close()
+					num_closed += 1
+			#END path matches 
+		#END for each path
+		return num_closed
+	#} END special purpose interface
