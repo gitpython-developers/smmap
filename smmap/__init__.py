d13e3aeb168645965720ddec1f469e05771563ef
Sebastian Thiel <byronimo@gmail.com>
2011-07-05 16:55:32 +0200

    updated changelog, bumped version

--- smmap/__init__.py
+++ smmap/__init__.py
@@ -3,7 +3,7 @@
 __author__ = "Sebastian Thiel"
 __contact__ = "byronimo@gmail.com"
 __homepage__ = "https://github.com/Byron/smmap"
-version_info = (0, 8, 0)
+version_info = (0, 8, 1)
 __version__ = '.'.join(str(i) for i in version_info)
 
 # make everything available in root package for convenience
