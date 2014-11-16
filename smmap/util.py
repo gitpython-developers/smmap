1cd3760be374521a7fafe2262e58c1703b71b9aa
Antoine Musso <hashar@free.fr>
2014-11-16 22:37:15 +0100

    Drop semicolon at end of statement
    Fix pep8: E703 statement ends with a semicolon

--- smmap/util.py
+++ smmap/util.py
@@ -42,11 +42,11 @@ def align_to_mmap(num, round_up):
     :param round_up: if True, the next higher multiple of page size is used, otherwise
         the lower page_size will be used (i.e. if True, 1 becomes 4096, otherwise it becomes 0)
     :return: num rounded to closest page"""
-    res = (num // ALLOCATIONGRANULARITY) * ALLOCATIONGRANULARITY;
+    res = (num // ALLOCATIONGRANULARITY) * ALLOCATIONGRANULARITY
     if round_up and (res != num):
         res += ALLOCATIONGRANULARITY
     #END handle size
-    return res;
+    return res
 
 
 def is_64_bit():
