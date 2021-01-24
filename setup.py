230725f81e0298d110f8b136e1a6a19c9105d961
Harmon <Harmon758@gmail.com>
2021-01-23 21:45:48 -0600

    Remove Development Status classifier comments in setup.py

--- setup.py
+++ setup.py
@@ -28,15 +28,7 @@ setup(
     zip_safe=True,
     python_requires=">=3.4",
     classifiers=[
-        # Picked from
-        #    http://pypi.python.org/pypi?:action=list_classifiers
-        #"Development Status :: 1 - Planning",
-        #"Development Status :: 2 - Pre-Alpha",
-        #"Development Status :: 3 - Alpha",
-        # "Development Status :: 4 - Beta",
         "Development Status :: 5 - Production/Stable",
-        #"Development Status :: 6 - Mature",
-        #"Development Status :: 7 - Inactive",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
